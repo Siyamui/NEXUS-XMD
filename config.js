bacf2b4d0230422930f5002600f18b7f8106918d
Siyamui <siyamkingff06@gmail.com>
2025-04-30 12:46:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "PkdrillerkSUGRSbQ#jTuLmIWWoO-twn1xKSiFbchWvErJQ4X1Bh_s1g9ixfI",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
